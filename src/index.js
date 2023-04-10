89277e53d3dac3dc042adb91bb8cc94bfd1ceaa1
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-04-11 02:51:44 +0300

    Added scroll to top button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import getRefs from './js/refs';
+import { scrollToTop, observer } from './js/back-to-top';
+
+// ##################################################################
+
 const { scrollToTopButton, target } = getRefs();
 
-import { scrollToTop, observer } from './js/back-to-top';
+// ##################################################################
 
 observer.observe(target);
 scrollToTopButton.addEventListener('click', scrollToTop);
+
+// ##################################################################
