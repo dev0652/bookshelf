b8e936f59cb3deb335979c6edbd56a0933bdd773
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-04-12 18:11:47 +0300

    tmp

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,12 @@
 export default function () {
   return {
     header: document.querySelector('header'),
+
+    // Scroll to top:
     scrollToTopButton: document.querySelector('.back-to-top'),
     toTopTarget: document.querySelector('.to-top-target'),
+
+    // Color scheme switcher:
+    colorSwitcher: document.querySelector('#color-scheme-switcher-checkbox'),
   };
 }
