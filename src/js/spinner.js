af1b2b239ab9c53671741c3e7e34c92e5a8db12b
UA-DmitriyS <tavluy86@gmail.com>
2023-04-13 18:28:58 +0300

    dd
    dd

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -9,10 +9,40 @@ export const spiner = {
   },
 
   start() {
-    return Notiflix.Loading.hourglass('Searching books...', this.options);
+    return Notiflix.Loading.hourglass('Ищем книгу...', this.options);
   },
 
   stop() {
     Notiflix.Loading.remove();
   },
 };
+// или что-то в этом роде?
+
+import Notiflix from 'notiflix';
+
+//  асинхронного запроса
+async function fetchData() {
+  Notiflix.Loading.hourglass('Loading...');
+
+  try {
+    const response = await fetch('');
+
+    // получаем ответ
+    const data = await response.json();
+
+    // скрыть спиннер после получения данных
+    Notiflix.Loading.remove();
+
+    // обработать данные
+    console.log(data);
+  } catch (error) {
+    // скрыть спиннер в случае ошибки
+    Notiflix.Loading.remove();
+
+    // обработать ошибку
+    console.error(error);
+  }
+}
+
+// вызвать функцию для выполнения запроса
+fetchData();
