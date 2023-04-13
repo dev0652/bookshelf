9e068a38a823cde65cdcb18afe14b8e58d881f29
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-04-13 22:29:05 +0300

    finished

--- src/js/home-bs/home-bs.js
+++ src/js/home-bs/home-bs.js
@@ -1,33 +1,31 @@
-import fetchBooks from "./fetchBooks.js";
-import  {createBookMarkup}  from "./markupBook.js";
+import fetchBooks from './fetchBooks.js';
+import { createBookMarkup } from './markupBook.js';
 
 const booksListFirstEl = document.querySelector('.books-list__first');
 const booksListSecEl = document.querySelector('.books-list__second');
 const booksListThirdEl = document.querySelector('.books-list__third');
 const booksListFourthEl = document.querySelector('.books-list__fourth');
 
-fetchBooks().then((data)=> {
-data.map((category)=> {
+fetchBooks()
+  .then(data => {
+    data.map(category => {
+      console.log(category);
 
-   console.log(category)
-
-   if(category.list_name === 'Combined Print and E-Book Fiction') {
-   return booksListFirstEl.innerHTML += createBookMarkup(category.books); 
-   }
-    
-else if(category.list_name === 'Combined Print and E-Book Nonfiction') {
-    return booksListSecEl.innerHTML += createBookMarkup(category.books);
-}
-
-else if(category.list_name === 'Hardcover Fiction') {
-    return booksListThirdEl.innerHTML += createBookMarkup(category.books);
-}
-
-else if(category.list_name === 'Hardcover Nonfiction') {
-   return booksListFourthEl.innerHTML += createBookMarkup(category.books);
-}
-
-})
-}).catch(err=> {
-    console.log(err)
-})
+      if (category.list_name === 'Combined Print and E-Book Fiction') {
+        return (booksListFirstEl.innerHTML += createBookMarkup(category.books));
+      } else if (
+        category.list_name === 'Combined Print and E-Book Nonfiction'
+      ) {
+        return (booksListSecEl.innerHTML += createBookMarkup(category.books));
+      } else if (category.list_name === 'Hardcover Fiction') {
+        return (booksListThirdEl.innerHTML += createBookMarkup(category.books));
+      } else if (category.list_name === 'Hardcover Nonfiction') {
+        return (booksListFourthEl.innerHTML += createBookMarkup(
+          category.books
+        ));
+      }
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
