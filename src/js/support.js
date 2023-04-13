3b33065be2edbd4d4c99a050af1b30ac866aef45
artemkravchenk0 <kravchenkoartem@ukr.net>
2023-04-14 00:39:29 +0300

    add

--- src/js/support.js
+++ src/js/support.js
@@ -1,3 +1,36 @@
-const supportList = document.querySelector('.support-list');
-const supportItem ='<li class="support-item"><a class="support-link" href=""></a></li>';
-supportList.innerHTML +=supportItem;
+import charities from "./charities"
+
+// console.log(charities);
+
+const list = document.querySelector('.support-list');
+// const item ='<li class="support-item"> ссылка на фонд <a class="support-link" href=""></a></li>';
+
+// console.log(item);
+
+// list.innerHTML += item;
+
+const html = charities.map((element, index) => {
+    return {
+        ...element,
+        index: (index+1).toString().padStart(2, '0')
+    }
+}).map((element) => {
+    return `<li class="support-item">
+        <span class="support-index">${element.index}</span>     
+        <a class="support-link" href="${element.url}" target="_blank">
+            <img
+                srcset="${element.img.normal} 1x, ${element.img.retina} 2x"
+                src="${element.img.normal}" alt="${element.title}">
+        </a>             
+    </li>`;
+});
+
+list.innerHTML = html.join('');
+console.log(html);
+
+
+// const str = charities.map((element, index) => {
+//     return '<li class="support-item"> ссылка на фонд <a class="support-link" href=""></a></li>'
+// });
+
+// console.log(str);
