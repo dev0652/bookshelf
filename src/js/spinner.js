494d2f319196c7c8ff3ee3ce801ed99148e6a3a3
UA-DmitriyS <tavluy86@gmail.com>
2023-04-13 23:57:14 +0300

    work....

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,28 +1,7 @@
 import Notiflix from 'notiflix';
 
-export const spiner = {
-  options: {
-    svgSize: '90px',
-    svgColor: 'rgba(0, 255, 27, 1)',
-    messageFontSize: '16px',
-    messageColor: 'rgba(0, 255, 27, 1)',
-  },
-
-  start() {
-    return Notiflix.Loading.hourglass('Ищем книгу...', this.options);
-  },
-
-  stop() {
-    Notiflix.Loading.remove();
-  },
-};
-// или что-то в этом роде?
-
-import Notiflix from 'notiflix';
-
-//  асинхронного запроса
 async function fetchData() {
-  Notiflix.Loading.hourglass('Loading...');
+  Notiflix.Loading.pulse('Зачекайте...');
 
   try {
     const response = await fetch('');
@@ -43,6 +22,5 @@ async function fetchData() {
     console.error(error);
   }
 }
-
 // вызвать функцию для выполнения запроса
 fetchData();
