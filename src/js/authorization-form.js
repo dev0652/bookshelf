129c9ce71e25cc01db2371322d190671c7623d33
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-04-13 23:38:03 +0300

    Create responsive sign in and sign up modal-form

--- src/js/authorization-form.js
+++ src/js/authorization-form.js
@@ -1,14 +1,60 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+const refs = {
+  // Sign up
+  openSignUpBtnEl: document.querySelector('#signUp-open-btn'),
+  signUpModalEl: document.querySelector('#sign-up-modal'),
+  closeSignUpBtnEl: document.querySelector('#signUp-modal-close-btn'),
+  openSignUpBtnSecondEl: document.querySelector('#signUp-open-btn-second'),
+  // Sign in
+  openSignInBtnEl: document.querySelector('#signIn-open-btn'),
+  signInModalEl: document.querySelector('#sign-in-modal'),
+  closeSignInBtnEl: document.querySelector('#signIn-modal-close-btn'),
+};
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.openSignUpBtnEl.addEventListener('click', handleClickOnSingUpOpenBtn);
+refs.closeSignUpBtnEl.addEventListener('click', handleClickOnSingUpCloseBtn);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+refs.openSignUpBtnSecondEl.addEventListener(
+  'click',
+  handleClickOnSingUpOpenBtn
+);
+
+refs.openSignInBtnEl.addEventListener('click', handleClickOnSingInOpenBtn);
+refs.closeSignInBtnEl.addEventListener('click', handleClickOnSingInCloseBtn);
+
+// Відкрити Sign up модалку
+function handleClickOnSingUpOpenBtn() {
+  refs.signUpModalEl.classList.add('open');
+  if (refs.signInModalEl.classList.contains('open')) {
+    handleClickOnSingInCloseBtn();
   }
-})();
+}
+
+// Закрити Sign up модалку
+function handleClickOnSingUpCloseBtn() {
+  refs.signUpModalEl.classList.remove('open');
+}
+
+// Закрити Sign up модалку при натисканні Esc
+window.addEventListener('keydown', evt => {
+  if (evt.key === 'Escape') {
+    refs.signUpModalEl.classList.remove('open');
+  }
+});
+
+// Відкрити Sign in модалку + закрити Sign up модалку
+function handleClickOnSingInOpenBtn() {
+  refs.signInModalEl.classList.add('open');
+  handleClickOnSingUpCloseBtn();
+}
+
+// Закрити Sign in модалку
+function handleClickOnSingInCloseBtn() {
+  refs.signInModalEl.classList.remove('open');
+}
+
+// Закрити Sign in модалку при натисканні Esc
+window.addEventListener('keydown', evt => {
+  if (evt.key === 'Escape') {
+    refs.signInModalEl.classList.remove('open');
+  }
+});
