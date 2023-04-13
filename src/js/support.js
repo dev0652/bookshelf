17e58c112672892ddbd24bed5822313b5b52280d
artemkravchenk0 <kravchenkoartem@ukr.net>
2023-04-13 18:41:03 +0300

    Update support.js
    2

--- src/js/support.js
+++ src/js/support.js
@@ -1,1 +1,3 @@
-
+const supportList = document.querySelector('.support-list');
+const supportItem ='<li class="support-item"><a class="support-link" href=""></a></li>';
+supportList.innerHTML +=supportItem;
