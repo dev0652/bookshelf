370ae0e78094e3c08fe8f32a8d60f4f34325d23e
Svyntsov Andriy <svincov_a@ukr.net>
2023-04-16 21:08:12 +0300

    16/04

--- src/js/header-height-adjust.js
+++ src/js/header-height-adjust.js
@@ -5,3 +5,14 @@ export default function () {
   const { height: pageHeaderHeight } = header.getBoundingClientRect();
   document.body.style.paddingTop = `${pageHeaderHeight}px`;
 }
+
+const currentLink = window.location.href;
+const links = document.querySelectorAll('.page-navigation-link ');
+
+for (let i = 0; i < links.length; i++) {
+  if (links[i].href === currentLink) {
+    links[i].classList.add('current');
+  } else {
+    links[i].classList.remove('currente');
+  }
+}
