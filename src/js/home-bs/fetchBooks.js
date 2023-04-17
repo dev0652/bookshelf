a578cf50cfc14ba1bfbc100cb9e968b45b3ecfd9
AnastasiiaSyplyvets <102216774+AnastasiiaSyplyvets@users.noreply.github.com>
2023-04-17 06:56:37 +0200

    removed comments

--- src/js/home-bs/fetchBooks.js
+++ src/js/home-bs/fetchBooks.js
@@ -1,22 +1,4 @@
 import axios from 'axios';
-
-// export default function fetchBooks(name) {
-
-
-//       return axios.get(`https://books-backend.p.goit.global/books/top-books`)
-//       .then(response =>{
-      
-
-//         return response.data;
-        
-//       })
-//       .catch(err=> {
-//         console.log(err)
-//       })
-//     }
-  
-
-    // Vitalii Code
     
    export default async function getTopBooksArray() {
       try {
