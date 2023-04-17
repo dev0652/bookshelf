837dd6520c03845497723648f9ddfa50656d694c
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-04-17 17:34:20 +0300

    Fixed conflicts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import getRefs from './js/refs';
+import './js/home-bs/home-bs';
+// import './js/home-categories';
+import './js/support';
+import './js/firebase/firebase';
+import './js/modal';
+import './js/burger-mobile';
+
+import './js/renderCategoriesList';
+import './js/activeCatBtnSwitcher';
+import './js/handleAllCategoriesClick';
+
 import headerHeightAdjust from './js/header-height-adjust';
 import { scrollToTop, toTopObserver } from './js/back-to-top';
-import './js/burger-mobile';
 
 import colorSwitcher from './js/color-scheme-switcher';
 // import { handleBookElClick } from './js/modal';
 
-// ##################################################################
+// ##########################################################
+
+import getRefs from './js/refs';
 
 // Add your ref here:
 const { scrollToTopButton, toTopTarget, authForm } = getRefs();
