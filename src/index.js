89324a92b26235fdaf590b6e53b03c45a4e025b6
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-04-18 03:55:18 +0300

    tmp

--- src/index.js
+++ src/index.js
@@ -2,42 +2,19 @@ import getRefs from './js/refs';
 const refs = getRefs();
 
 import './js/home-bs/home-bs';
-import './js/home-categories';
-import './js/support';
-import './js/firebase/firebase';
-import './js/modal';
-import './js/burger-mobile';
 
+// Sidebar
+import './js/home-categories';
 import './js/renderCategoriesList';
 import './js/activeCatBtnSwitcher';
 import './js/handleAllCategoriesClick';
+import './js/support';
+
 import './js/renderSelectedBooks';
 
+import './js/modal';
+
 import './js/header-height-adjust';
-
-import { scrollToTop, toTopObserver } from './js/back-to-top';
-// Activate scroll-to-top button
-res.toTopObserver.observe(toTopTarget);
-refs / scrollToTopButton.addEventListener('click', scrollToTop);
-
-// import './js/color-scheme-switcher';
-import colorSwitcher from './js/color-scheme-switcher';
-
-// Sidebar
-import './js/support';
-import './js/renderCategoriesList';
-// // import renderCategories from './js/renderCategoriesList';
-// // renderCategories();
-import './js/home-categories';
-import './js/activeCatBtnSwitcher';
-
-// // import { handleBookElClick } from './js/modal';
-
-// // Best Seller Books
-// import './js/home-bs/home-bs';
-
-// Modal window
-// import './js/modal';
 
 // Authorization
 import './js/firebase/firebase';
@@ -46,6 +23,14 @@ authForm.addEventListener('submit', validateOnSubmit);
 
 // Mobile menu
 import './js/burger-mobile';
+
+import { scrollToTop, toTopObserver } from './js/back-to-top';
+// Activate scroll-to-top button
+res.toTopObserver.observe(toTopTarget);
+refs / scrollToTopButton.addEventListener('click', scrollToTop);
+
+import './js/color-scheme-switcher';
+// import colorSwitcher from './js/color-scheme-switcher';
 
 // import './js/handleAllCategoriesClick';
 // // import './js/renderSelectedBooks';
