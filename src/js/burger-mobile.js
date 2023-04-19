2f9e04905b857120c687cf43259083dadcca6aa2
Svyntsov Andriy <svincov_a@ukr.net>
2023-04-19 17:43:00 +0300

    header_19/04

--- src/js/burger-mobile.js
+++ src/js/burger-mobile.js
@@ -1,21 +1,36 @@
+// const refs = {
+//     openMenuBtn: document.querySelector('[data-menu-open]'),
+//     closeMenuBtn: document.querySelector('[data-menu-close]'),
+//     menu: document.querySelector('[data-menu]'),
+//   };
+
+//   refs.openMenuBtn.addEventListener('click', toggleModal);
+//   refs.closeMenuBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.menu.classList.toggle('is-open');
+//     refs.openMenuBtn.classList.toggle('is-hidden');
+//     refs.closeMenuBtn.classList.toggle('is-hidden');
+
+//     if (refs.openMenuBtn.classList.contains('is-hidden')) {
+//       document.body.style.overflow = 'hidden';
+//     }
+//     if (refs.closeMenuBtn.classList.contains('is-hidden')) {
+//       document.body.style.overflow = '';
+//     }
+//   };
+
 const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
-  };
-
-  refs.openMenuBtn.addEventListener('click', toggleModal);
-  refs.closeMenuBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.menu.classList.toggle('is-open');
-    refs.openMenuBtn.classList.toggle('is-hidden');
-    refs.closeMenuBtn.classList.toggle('is-hidden');
-
-    if (refs.openMenuBtn.classList.contains('is-hidden')) {
-      document.body.style.overflow = 'hidden';
-    }
-    if (refs.closeMenuBtn.classList.contains('is-hidden')) {
-      document.body.style.overflow = '';
-    }
-  };
+  openMenuBtn: document.querySelector('[data-menu-open]'),
+  menu: document.querySelector('[data-menu]'),
+};
+refs.openMenuBtn.addEventListener('click', toggleModal);
+function toggleModal() {
+  refs.menu.classList.toggle('is-open');
+  refs.openMenuBtn.classList.toggle('is-hidden');
+  if (refs.openMenuBtn.classList.contains('is-hidden')) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
+}
