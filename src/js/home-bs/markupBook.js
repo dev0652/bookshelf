792bdf2cc99a3213b02323323ffd7906094c1066
AnastasiiaSyplyvets <102216774+AnastasiiaSyplyvets@users.noreply.github.com>
2023-04-20 07:25:18 +0200

    Final2
    final2

--- src/js/home-bs/markupBook.js
+++ src/js/home-bs/markupBook.js
@@ -1,15 +1,34 @@
-export  const createBookMarkup =  (book)=> {
+// export  const createBookMarkup =  (book)=> {
 
-    const markup =   `
-       <li class="book-item">
-       <img  src="${book.book_image}"  loading="lazy"  class="book-image" data_id=${book._id}/>
-       <h3 class="book-title">${book.title}</h3>
-       <p class="book-author"> ${book.author}
-        </p>
-        </li>`
+//     const markup =   `
+//        <li class="book-item">
+//        <img  src="${book.book_image}"  loading="lazy"  class="book-image" data_id=${book._id}/>
+//        <h3 class="book-title">${book.title}</h3>
+//        <p class="book-author"> ${book.author}
+//         </p>
+//         </li>`
        
     
-   return markup;
+//    return markup;
    
    
-   }
+//    }
+
+   export function createBookMarkup(book) {
+    const markup = `
+                          <a class="book-item-link" href="" aria-label="Book thumbnail">
+                              <div class="book-item-thumb">
+                                  <img class="book-image" src="${book.book_image}"  loading="lazy" data_id=${book._id} />
+                                  <div class="book-image-overlay" data_id=${book._id} >
+                                      <p class="book-image-overlay-text">Quick view</p>
+                                  </div>
+                              </div>
+                              <div class="book-item-meta">
+                                  <h3 class="book-title">${book.title}</h3>
+                                     <p class="book-author"> ${book.author}</p>
+                              </div>
+                          </a>
+          `;
+  
+    return markup;
+  }
