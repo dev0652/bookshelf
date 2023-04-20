e808abff8345674c463a2691c158df6eec89ac0e
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-04-20 13:38:24 +0000

    123

--- src/js/renderSelectedCategory.js
+++ src/js/renderSelectedCategory.js
@@ -15,7 +15,25 @@ export function renderPage(data) {
         book_image_width,
         book_image_height,
       }) => {
-        return `<li class="book-item-cat"><img class="book-image" src="${book_image}" alt="Cover of ${title}"><h3 class="book-title">${title}</h3><h4 class="book-author">${author}</h4></li>`;
+        return `<li class="book-item-cat">
+        
+                <a class="book-item-link" href="" aria-label="Book thumbnail">
+							<div class="book-item-thumb">
+
+								<img class="book-image" src="${book_image}"  loading="lazy" data_id=${_id} />
+
+								<div class="book-image-overlay" data_id=${_id} >
+									<p class="book-image-overlay-text">Quick view</p>
+								</div>
+							</div>
+
+							<div class="book-item-meta">
+								<h3 class="book-title">${title}</h3>
+       					        <p class="book-author"> ${author}</p>
+							</div>
+						</a>
+        
+        </li>`;
       }
     )
     .join('');
