6fa408aa955d95ece92412a2fd18531e228fa019
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-04-20 03:11:17 +0300

    adds classes on Yevhen request

--- src/js/renderSelectedCategory.js
+++ src/js/renderSelectedCategory.js
@@ -4,8 +4,19 @@ const { selectedBooksListEl } = getRefs();
 export function renderPage(data) {
   selectedBooksListEl.innerHTML = '';
   return data
-    .map(({ _id, title, author, description, list_name, book_image }) => {
-      return `<li class="selected-book"><img src="${book_image}" alt="Cover of ${title}"><h3>${title}</h3><h4>${author}</h4></li>`;
-    })
+    .map(
+      ({
+        _id,
+        title,
+        author,
+        description,
+        list_name,
+        book_image,
+        book_image_width,
+        book_image_height,
+      }) => {
+        return `<li class="book-item-cat"><img class="book-image" src="${book_image}" alt="Cover of ${title}"><h3 class="book-title">${title}</h3><h4 class="book-author">${author}</h4></li>`;
+      }
+    )
     .join('');
 }
