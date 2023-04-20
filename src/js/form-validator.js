49cb5e5ec76c12c77fedec48a8e69cdc36e2b706
Viacheslav Plevako <cheryec@ukr.net>
2023-04-20 14:12:29 +0300

    підключив валідацію

--- src/js/form-validator.js
+++ src/js/form-validator.js
@@ -1,16 +1,18 @@
 import Notiflix from 'notiflix';
 
 import getRefs from './refs';
-const { authForm, nameField, emailField, passwordField, authSubmitBtn } =
-  getRefs();
+const { authForm, nameField, emailField, passwordField } = getRefs();
+
+const emailRegExp =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+const nameRegExp = /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/;
 
 // Обработчик отправки формы
 
-authForm.addEventListener('focusout', validateOnSubmit);
+authForm.addEventListener('submit', validateOnSubmit);
 
 export default function validateOnSubmit(e) {
-  e.preventDefault();
-
   // Убираем пробелы
 
   const name = nameField.value.trim();
@@ -18,26 +20,29 @@ export default function validateOnSubmit(e) {
   const password = passwordField.value.trim();
 
   // Проверяем, что все поля заполнены
+  const testName = name.length === 0 || nameRegExp.test(name);
+  const testEmail = email.length === 0 || emailRegExp.test(email);
+  const testPassword = password.length > 5;
 
-  if (name === '' || email === '' || password === '') {
-    Notiflix.Notify.warning('Please fill in all fields');
-
+  if (!testName) {
+    e.preventDefault();
+    Notiflix.Notify.failure('Please enter a valid name');
     return;
   }
 
   // проверяем, что адрес электронной почты имеет правильный формат
-  if (!/\S+@\S+\.\S+/.test(email)) {
+  else if (!testEmail) {
+    e.preventDefault();
     Notiflix.Notify.failure('Please enter a valid email');
     return;
   }
 
   // проверяем, что пароль имеет длину не менее 5 символов
-  if (password.length < 5) {
+  else if (!testPassword) {
+    e.preventDefault();
     Notiflix.Notify.failure('Password has to be at least 5 characters long');
     return;
   }
-
   //проверки прошли успешно => отправляем форму
-  Notiflix.Notify.success('Submitted successfully');
-  authForm.reset();
+  return true;
 }
