e4011e606738897c189b4024508c69cc687a3e75
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-04-21 22:26:32 +0300

    inserts template string in func

--- src/js/home-bs/markupBook.js
+++ src/js/home-bs/markupBook.js
@@ -1,22 +1,25 @@
-export function createBookMarkup(book) {
-  const markup = `
-						<a class="book-item-link" href="" aria-label="Book thumbnail">
-							<div class="book-item-thumb">
-
-								<img class="book-image" src="${book.book_image}"  loading="lazy" data_id=${book._id} alt="book cover"/>
-
-								<div class="book-image-overlay" data_id=${book._id} >
-									<p class="book-image-overlay-text">Quick view</p>
-								</div>
-							</div>
-
-							<div class="book-item-meta">
-								<h3 class="book-title">${book.title}</h3>
-       					        <p class="book-author"> ${book.author}</p>
-							</div>
-						</a>
-
-        `;
-
-  return markup;
+export function createBookMarkup({
+  _id,
+  title,
+  author,
+  description,
+  list_name,
+  book_image,
+  book_image_width,
+  book_image_height,
+}) {
+  return `
+    <a class="book-item-link" href="" aria-label="Book thumbnail">
+      <div class="book-item-thumb">
+        <img class="book-image" src="${book_image}" loading="lazy" data_id=${_id} />
+        <div class="book-image-overlay" data_id=${_id}>
+          <p class="book-image-overlay-text">Quick view</p>
+        </div>
+      </div>
+      <div class="book-item-meta">
+        <h3 class="book-title">${title}</h3>
+        <p class="book-author">${author}</p>
+      </div>
+    </a>
+  `;
 }
