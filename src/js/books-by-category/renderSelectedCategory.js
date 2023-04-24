5272a4374bbdf71d27958ede9144f73538875b26
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-04-24 03:03:13 +0300

    code refactoring

--- src/js/books-by-category/renderSelectedCategory.js
+++ src/js/books-by-category/renderSelectedCategory.js
@@ -6,6 +6,5 @@ import { makeCategoryBooksMarkup } from '../books-by-category/allBooksMarkup';
 // Render content for an individual category of books
 export function renderBooksByCategory(data) {
   const markup = makeCategoryBooksMarkup(data);
-
   renderingContainer.innerHTML = markup;
 }
